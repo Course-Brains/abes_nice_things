4cce2cd901aea497f3538e19efc6e97a11461db9
Course Brains <99380906+Course-Brains@users.noreply.github.com>
2025-03-04 19:10:57 -0800

    Moved file_comm here from net_test
    Moved the file_comm code to this as a binary from net_test.
    Net test inherently is not meant for more permanent code, and this seems
    to be more permanent, so I moved it here, where more permanent random,
    but useful, code goes.

---
+++ src/bin/file_comm/formats.rs
@@ -0,0 +1,151 @@
+use std::net::TcpStream;
+use abes_nice_things::{ToBinary, FromBinary};
+use crate::{Settings, Mode};
+
+pub type FormatID = u32;
+pub const HIGHEST: FormatID = 0;
+
+pub fn hand_shake(stream: TcpStream, settings: Settings) -> Result<(), String> {
+    println!("Beginning format handshake");
+    //////////////////////////////////////////////////////////////
+    // The person sending will be the one to suggest the format //
+    //////////////////////////////////////////////////////////////
+    // We are assuming that backwards compatability exists      //
+    //////////////////////////////////////////////////////////////
+    match settings.mode {
+        Mode::Recv => recv_hand_shake(stream, settings),
+        Mode::Send => send_hand_shake(stream, settings)
+    }
+}
+fn send_hand_shake(mut stream: TcpStream, settings: Settings) -> Result<(), String> {
+    println!("Sending suggested format: {}", settings.get_format());
+    settings.get_format().to_binary(&mut stream);
+    // Format is decided by the reciever
+    let format = FormatID::from_binary(&mut stream);
+    println!("Decided to use format: {format}\nFormat handshake done");
+    match format {
+        0 => f0::send(stream, settings),
+        _ => return Err("Invalid format given by other".to_string())
+    }
+    Ok(())
+}
+fn recv_hand_shake(mut stream: TcpStream, settings: Settings) -> Result<(), String> {
+    println!("Waiting for suggested format");
+    let other_highest = FormatID::from_binary(&mut stream);
+    println!("Suggestion: {other_highest}");
+    let format = {
+        // We are able to process their highest format
+        if other_highest <= settings.get_format() {
+            println!("Accepting suggestion");
+            other_highest
+        }
+        // We cannot match them, so they have to match us
+        else {
+            println!("Suggestion is impossible, sending alternative");
+            settings.get_format()
+        }
+    };
+    Ok::<u32, String>(format).to_binary(&mut stream);
+    println!("Format handshake done");
+    match format {
+        0 => f0::recv(stream),
+        _ => unreachable!()
+    };
+    Ok(())
+}
+mod f0 {
+    // Format:
+    // Length of name(u32)
+    // name
+    // data
+    use std::{
+        net::TcpStream,
+        fs::File,
+        path::PathBuf,
+        io::{Read, Write}
+    };
+    use crate::Settings;
+    use abes_nice_things::{input, Input, ToBinary, FromBinary};
+
+    static mut TO_SEND: Option<PathBuf> = None;
+    pub fn send(mut stream: TcpStream, settings: Settings) {
+        let file;
+        #[allow(static_mut_refs)]// compiler is wrong
+        let path = match unsafe { TO_SEND.clone() } {
+            Some(path) => {
+                file = File::open(&path).unwrap();
+                path
+            },
+            None => {
+                loop {
+                    println!("What file do you want to send?");
+                    let path = input();
+                    match File::open(&path) {
+                        
+                        Ok(file_in) => {
+                            println!("Valid file");
+                            file = file_in;
+                            if let Some(_) = settings.host {
+                                if "y".to_string() == <Input>::yn()
+                                    .msg("Do you want to use this for subsequent requests?y/n")
+                                .get().unwrap() {
+                                    unsafe { TO_SEND = Some(PathBuf::from(&path)) }
+                                }
+                            }
+                            break PathBuf::from(path)
+                        }
+                        Err(error) => eprintln!("Failed to identify file validity: {error}")
+                    }
+                }
+            }
+        };
+        let path = path.file_name().expect("Failed to get file name").to_str().unwrap();
+        let len = file.metadata().expect("Failed to get file metadata").len();
+
+        println!("Sending metadata");
+        (path.len() as u32).to_binary(&mut stream);
+        stream.write_all(path.as_bytes()).unwrap();
+        println!("Sending file");
+        transfer(file, stream, len, 1000);
+        println!("File sent")
+    }
+    pub fn recv(mut stream: TcpStream) {
+        println!("Getting metadata");
+        let name_len = u32::from_binary(&mut stream);
+        let mut buf = vec![0; name_len as usize];
+        stream.read_exact(&mut buf).unwrap();
+        let name = String::from_utf8(buf).unwrap();
+        if "n" == match stream.peer_addr() {
+            Ok(addr) => <Input>::yn().msg(
+                &format!(
+                    "Are you sure you want to accept {name} from {addr}?y/n"
+                )
+            ).get().unwrap(),
+            Err(_) => <Input>::yn().msg(
+                &format!(
+                    "Are you sure you want to accept {name} from unknown?y/n"
+                )
+            ).get().unwrap()
+        } {
+            return
+        }
+        let mut buf = Vec::new();
+        println!("Getting data");
+        stream.read_to_end(&mut buf).unwrap();
+        println!("Writing to file");
+        std::fs::write(name, buf).unwrap();
+        println!("Done")
+        
+    }
+    fn transfer(mut from: impl Read, mut to: impl Write, mut len: u64, interval: usize) {
+        while len > interval as u64 {
+            let mut buf = vec![0_u8; interval];
+            from.read_exact(&mut buf).unwrap();
+            to.write_all(&buf).unwrap();
+            len -= interval as u64;
+        }
+        let mut buf = vec![0; len.try_into().unwrap()];
+        from.read_exact(&mut buf).unwrap();
+        to.write_all(&buf).unwrap();
+    }
+}
---
+++ src/bin/file_comm/main.rs
@@ -0,0 +1,124 @@
+use std::net::*;
+use abes_nice_things::{input, Input};
+mod formats;
+
+fn main() {
+    let settings = Settings::new();
+    if let None = settings {
+        return
+    }
+    let settings = settings.unwrap();
+    match settings.host {
+        Some(port) => host(port, settings),
+        None => connect(settings),
+    }
+}
+fn host(port: u16, settings: Settings) {
+    println!("Listening...");
+    for connection in TcpListener::bind(
+        (Ipv4Addr::UNSPECIFIED, port)
+    ).expect("Failed to bind to port").incoming() {
+        println!("Incoming connection");
+        match connection {
+            Ok(stream) => {
+                if let Err(error) = formats::hand_shake(stream, settings) {
+                    eprintln!("{error}")
+                }
+            }
+            Err(error) => eprintln!("Failed to connect: {error}")
+        }
+    }
+}
+fn connect(settings: Settings) {
+    loop {
+        match settings.mode {
+            Mode::Send => println!("What port:addr do you want to send a file to?"),
+            Mode::Recv => println!("What port:addr do you want to recieve a file from?")
+        }
+        match TcpStream::connect(input()) {
+            Ok(stream) => {
+                if let Err(error) = formats::hand_shake(stream, settings) {
+                    eprintln!("{error}")
+                }
+            }
+            Err(error) => eprintln!("Failed to connect: {error}")
+        }
+    }
+}
+#[derive(Clone, Copy)]
+struct Settings {
+    mode: Mode,
+    host: Option<u16>,
+    overide: Option<formats::FormatID>
+}
+impl Settings {
+    const HELP: &str = include_str!("help.txt");
+    fn new() -> Option<Settings> {
+        let mut out = Settings::default();
+        let mut mode = None;
+        let mut args = std::env::args();
+        while let Some(arg) = args.next() {
+            match arg.as_str() {
+                "help" => {
+                    println!("{}", Settings::HELP);
+                    return None
+                }
+                "--recv" => mode = Some(Mode::Recv),
+                "--send" => mode = Some(Mode::Send),
+                "--host" => {
+                    out.host = Some(
+                        args.next().expect("Need a port after --host")
+                            .parse::<u16>().expect("Need a port after --host")
+                    );
+                },
+                "--override" => {
+                    out.overide = Some(
+                        args.next().expect("Need a format id after --override")
+                            .parse::<formats::FormatID>().expect("Need for a format id after --override")
+                    );
+                    assert!(
+                        out.overide.unwrap() > formats::HIGHEST,
+                        "Need a valid format id after --override"
+                    )
+                }
+                _ => {}
+            }
+        }
+        match mode {
+            Some(mode) => out.mode = mode,
+            None => {
+                match <Input>::new().cond(&|string| {
+                    match string.as_str() {
+                        "recv"|"r"|"send"|"s" => Ok(true),
+                        _ => Ok(false)
+                    }
+                }).msg("").get().unwrap().as_str() {
+                    "recv"|"r" => out.mode = Mode::Recv,
+                    "send"|"s" => out.mode = Mode::Send,
+                    _ => unreachable!()
+                }
+            }
+        }
+        return Some(out);
+    }
+    fn get_format(&self) -> formats::FormatID {
+        match self.overide {
+            Some(format) => format,
+            None => formats::HIGHEST
+        }
+    }
+}
+impl Default for Settings {
+    fn default() -> Self {
+        Settings {
+            mode: Mode::Recv,
+            host: None,
+            overide: None
+        }
+    }
+}
+#[derive(Clone, Copy)]
+enum Mode {
+    Send,
+    Recv
+}
